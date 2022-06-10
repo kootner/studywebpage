e80f03ddbf423e98d3a5f710595a746e5b8c5231
WE-DA-pluggg <79959576+WE-DA-pluggg@users.noreply.github.com>
2022-06-10 20:35:17 +0900

    코멘트 기본 로직

--- src/main/java/com/sparta/studywebpage/service/CommentService.java
+++ src/main/java/com/sparta/studywebpage/service/CommentService.java
@@ -5,21 +5,48 @@ import com.sparta.studywebpage.exception.CustomException;
 import com.sparta.studywebpage.exception.ErrorCode;
 import com.sparta.studywebpage.model.Comment;
 import com.sparta.studywebpage.model.Study;
+import com.sparta.studywebpage.model.User;
 import com.sparta.studywebpage.repository.CommentRepository;
 import com.sparta.studywebpage.repository.StudyRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class CommentService {
     private CommentRepository commentRepository;
     private StudyRepository studyRepository;
-    public void createComment(Long studyId, CommentRequestDto commentsRequestDto,String user) {
+
+
+    @Transactional
+    public void createComment(Long studyId, CommentRequestDto commentsRequestDto, User user) {
         Comment comment = new Comment(commentsRequestDto);
-        Study study = studyRepository.findById(studyId).orElseThrow(()->new CustomException(ErrorCode.NOT_FOUND_COMMENT));
+        Study study = studyRepository.findById(studyId).orElseThrow(()->new CustomException(ErrorCode.NOT_FOUND_STUDY));
 
         comment.setStudy(study);
-        comment.setUser();
+        comment.setUser(user);
+
+        commentRepository.save(comment);
+
+    }
+    @Transactional
+    public void updateComment(Long commentId, CommentRequestDto commentRequestDto, User user) {
+        Comment comment = commentRepository.findById(commentId).orElseThrow(()->new CustomException(ErrorCode.NOT_FOUND_COMMENT));
+        if(user!= comment.getUser()){
+            throw new CustomException(ErrorCode.WRONG_USER);
+        }
+        comment.setComment(commentRequestDto.getComment());
+        commentRepository.save(comment);
+    }
+
+    @Transactional
+    public void deleteComment(Long commentId, User user) {
+        Comment comment = commentRepository.findById(commentId).orElseThrow(()->new CustomException(ErrorCode.NOT_FOUND_COMMENT));
+        if(user!= comment.getUser()){
+            throw new CustomException(ErrorCode.WRONG_USER);
+        }
+        commentRepository.delete(comment);
     }
 }
