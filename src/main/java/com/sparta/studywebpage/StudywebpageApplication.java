d91f336598c24843ee83006ba32a3c26ce8cbb46
hyungu_kang <refomto@naver.com>
2022-06-10 13:35:06 +0900

    Initial commit

---
+++ src/main/java/com/sparta/studywebpage/StudywebpageApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.studywebpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudywebpageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudywebpageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/studywebpage/StudywebpageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.studywebpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudywebpageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
