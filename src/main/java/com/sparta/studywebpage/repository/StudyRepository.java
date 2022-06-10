1abd192d7171aa5e3cda7be8eaaef384d14c4ac5
WE-DA-pluggg <79959576+WE-DA-pluggg@users.noreply.github.com>
2022-06-10 16:33:08 +0900

    다듬기

--- src/main/java/com/sparta/studywebpage/repository/StudyRepository.java
+++ src/main/java/com/sparta/studywebpage/repository/StudyRepository.java
@@ -3,5 +3,9 @@ package com.sparta.studywebpage.repository;
 import com.sparta.studywebpage.model.Study;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface StudyRepository extends JpaRepository<Study, Long> {
+
+    Optional<Study> findById(Long id);
 }
