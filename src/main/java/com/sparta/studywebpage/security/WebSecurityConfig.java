dcda47e27b9f8194394d4c34a798802af3308497
kootner <refomto@naver.com>
2022-06-11 21:29:16 +0900

    충돌 부분 재수정

--- src/main/java/com/sparta/studywebpage/security/WebSecurityConfig.java
+++ src/main/java/com/sparta/studywebpage/security/WebSecurityConfig.java
@@ -49,10 +49,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
-<<<<<<< HEAD
 
-=======
->>>>>>> origin/JungHoon
     @Override
     public void configure(AuthenticationManagerBuilder auth) {
         auth
@@ -154,10 +151,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         skipPathList.add("POST,/user/signup");
 
         skipPathList.add("GET,/");
-<<<<<<< HEAD
-//        skipPathList.add("");
-=======
->>>>>>> origin/JungHoon
 
         FilterSkipMatcher matcher = new FilterSkipMatcher(
                 skipPathList,
