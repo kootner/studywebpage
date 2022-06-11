33709d5291097af8eb32898ab2ffa0bd1d3fdbd3
WE-DA-pluggg <79959576+WE-DA-pluggg@users.noreply.github.com>
2022-06-11 14:06:05 +0900

    업데이트

--- src/main/java/com/sparta/studywebpage/service/MainPageService.java
+++ src/main/java/com/sparta/studywebpage/service/MainPageService.java
@@ -2,17 +2,15 @@ package com.sparta.studywebpage.service;
 
 
 import com.sparta.studywebpage.dto.MainPageSearchDto;
-import com.sparta.studywebpage.model.Study;
+
 import com.sparta.studywebpage.repository.StudyRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
+
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.HashMap;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -23,21 +21,14 @@ public class MainPageService {
 
 
     @Transactional
-    public  Page<MainPageSearchDto> getAllMainPageByCategory(int page, int size, String sortBy, boolean isAsc){
-        Sort.Direction direction = isAsc ? Sort.Direction.ASC : Sort.Direction.DESC;
-        Sort sort = Sort.by(direction, sortBy);
-        Pageable pageable = PageRequest.of(page, size, sort);
+    public  List<MainPageSearchDto> getAllMainPageByCategory(){
 
-        return studyRepository.getAllStudies(pageable);
+        return studyRepository.getAllStudies();
     }
 
     @Transactional
-    public  Page<MainPageSearchDto> getMainPageByCategory(int page, int size, String sortBy, boolean isAsc, String category) {
-        Sort.Direction direction = isAsc ? Sort.Direction.ASC : Sort.Direction.DESC;
-        Sort sort = Sort.by(direction, sortBy);
-        Pageable pageable = PageRequest.of(page, size, sort);
-
-        return studyRepository.getStudies(category,pageable);
+    public List<MainPageSearchDto> getMainPageByCategory(String category) {
+        return studyRepository.getStudies(category);
 
     }
 }
