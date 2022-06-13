dc546b0af73cc4429be5b7e419cd7e9416f344c2
junghoonkim96 <0527wj@gmail.com>
2022-06-13 11:27:57 +0900

    Revert "로그인 메세지"
    This reverts commit 6ba528ef72224b351363d2e27baf39b45bb730a3.

--- src/main/java/com/sparta/studywebpage/security/FormLoginSuccessHandler.java
+++ src/main/java/com/sparta/studywebpage/security/FormLoginSuccessHandler.java
@@ -2,8 +2,6 @@ package com.sparta.studywebpage.security;
 
 
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.sparta.studywebpage.dto.LoginSuccessDto;
 import com.sparta.studywebpage.security.jwt.JwtTokenUtils;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
@@ -16,8 +14,6 @@ public class FormLoginSuccessHandler extends SavedRequestAwareAuthenticationSucc
     public static final String AUTH_HEADER = "Authorization";
     public static final String TOKEN_TYPE = "BEARER";
 
-    private ObjectMapper objectMapper;
-
     @Override
     public void onAuthenticationSuccess(final HttpServletRequest request, final HttpServletResponse response,
                                         final Authentication authentication) throws IOException {
@@ -25,13 +21,6 @@ public class FormLoginSuccessHandler extends SavedRequestAwareAuthenticationSucc
         // Token 생성
         final String token = JwtTokenUtils.generateJwtToken(userDetails);
         response.addHeader(AUTH_HEADER, TOKEN_TYPE + " " + token);
-        response.setContentType("application/json");
-        response.setCharacterEncoding("utf-8");
-        LoginSuccessDto loginSuccessDto = new LoginSuccessDto();
-        loginSuccessDto.setSuccessMessage("로그인 성공입니다.");
-
-        String result = objectMapper.writeValueAsString(loginSuccessDto);
-        response.getWriter().write(result);
 
     }
 
