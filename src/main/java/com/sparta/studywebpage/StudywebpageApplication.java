4fe5c61c79c2d7d0e4e8752ff431ae11fb36d0bc
kootner <refomto@naver.com>
2022-06-15 20:44:48 +0900

    s3 설정 삭제

--- src/main/java/com/sparta/studywebpage/StudywebpageApplication.java
+++ src/main/java/com/sparta/studywebpage/StudywebpageApplication.java
@@ -1,7 +1,7 @@
 package com.sparta.studywebpage;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -11,8 +11,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class StudywebpageApplication {
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder(StudywebpageApplication.class)
-                .run(args);
+        SpringApplication.run(StudywebpageApplication.class,args);
+
     }
 }
 
