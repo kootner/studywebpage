b0de5ffd75a888a8a0cf423e3e41a5a1f6307046
kootner <refomto@naver.com>
2022-06-15 13:08:50 +0900

    이미지 업로드 설정 1

--- src/main/java/com/sparta/studywebpage/StudywebpageApplication.java
+++ src/main/java/com/sparta/studywebpage/StudywebpageApplication.java
@@ -2,6 +2,7 @@ package com.sparta.studywebpage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -9,9 +10,13 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableJpaAuditing
 public class StudywebpageApplication {
+    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "classpath:aws.yml";
 
     public static void main(String[] args) {
-        SpringApplication.run(StudywebpageApplication.class, args);
+        new SpringApplicationBuilder(StudywebpageApplication.class)
+                .properties(APPLICATION_LOCATIONS)
+                .run(args);
     }
-
 }
+
