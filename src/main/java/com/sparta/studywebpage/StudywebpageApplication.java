bed37b3396630e52c9ff1c932ccb7b4a1298bd7d
kootner <refomto@naver.com>
2022-06-15 16:46:02 +0900

    S3 연결 기능 추가

--- src/main/java/com/sparta/studywebpage/StudywebpageApplication.java
+++ src/main/java/com/sparta/studywebpage/StudywebpageApplication.java
@@ -1,6 +1,5 @@
 package com.sparta.studywebpage;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -10,6 +9,9 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableJpaAuditing
 public class StudywebpageApplication {
+    static {
+        System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
+    }
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(StudywebpageApplication.class)
