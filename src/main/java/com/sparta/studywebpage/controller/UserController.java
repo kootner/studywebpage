b8673c60b8de3215d4bbeb7980ebb5715c5736a8
WE-DA-pluggg <79959576+WE-DA-pluggg@users.noreply.github.com>
2022-06-15 22:44:35 +0900

    Update UserController.java

--- src/main/java/com/sparta/studywebpage/controller/UserController.java
+++ src/main/java/com/sparta/studywebpage/controller/UserController.java
@@ -10,6 +10,7 @@ import com.sparta.studywebpage.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
@@ -17,7 +18,7 @@ import javax.servlet.http.HttpServletResponse;
 @Slf4j
 @RequestMapping("/user")
 @RequiredArgsConstructor
-@RestController
+@Controller
 public class UserController {
     private final UserService userService;
 
@@ -30,9 +31,9 @@ public class UserController {
     }
 
     @GetMapping("/kakao/callback")
-    public ResponseEntity<KakaoUserResponseDto> kakaoLogin(@RequestParam String code, HttpServletResponse response) throws JsonProcessingException {
+    public String kakaoLogin(@RequestParam String code, HttpServletResponse response) throws JsonProcessingException {
         KakaoUserResponseDto kakaoUserResponseDto = kakaoUserService.kakaoLogin(code, response);
-        return ResponseEntity.ok().body(kakaoUserResponseDto);
+        return "redirect:http://localhost:3000";
     }
 
 
